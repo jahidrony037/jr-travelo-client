7ec662774efa83046a2a110ea966db09e5fc2a54
Jahid Hasan Rony <jahidrony162@gmail.com>
2024-04-26 00:51:33 +0600

    navbar modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import { Link, NavLink } from "react-router-dom";
 import logo from "../../public/logo.svg";
+import useAuth from "../hooks/useAuth";
 import "./Navbar.css";
 const Navbar = () => {
+  const { user } = useAuth();
   const navLinks = (
     <>
       <li>
@@ -112,6 +114,49 @@ const Navbar = () => {
               </span>
             </button>
           )} */}
+
+          {!user ? (
+            <Link
+              to="/login"
+              className="px-5 py-2 relative rounded  group overflow-hidden font-medium bg-[#1ec6b6] text-white inline-block border-[1px] "
+            >
+              <span className="absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out transform translate-y-0 bg-[#ff4a52]  group-hover:h-full opacity-90 group-hover:border-[#ff4a52]"></span>
+              <span className="relative group-hover:text-white font-bold">
+                Login
+              </span>
+            </Link>
+          ) : (
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS Navbar component"
+                    title="user-image"
+                    src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                  />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 hidden"
+              >
+                <li>
+                  <Link to="" className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </Link>
+                </li>
+
+                <li>
+                  <button className="btn btn-success ">Logout</button>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </nav>
